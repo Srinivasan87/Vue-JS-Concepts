1409c43696f0420834e180f852cb63a7389629fa
Srinivasan Jayaraman <45791344+Srinivasan87@users.noreply.github.com>
2019-04-07 04:26:40 -0400

    12 Keyboard Events
    12 Keyboard Events

---
+++ Basic Concepts/12 Keyboard Events/index.js
@@ -0,0 +1,20 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        counter: 0,
+        x:0,
+        y:0
+    },
+    methods:{
+        increase: function(step,event){
+            this.counter += step;
+        },
+        updatecoordinats:function(){
+            this.x=event.clientX;
+            this.y=event.clientY;
+        },
+        alertMe:function(step,event){
+            alert('Alert');
+        },
+    }
+});
