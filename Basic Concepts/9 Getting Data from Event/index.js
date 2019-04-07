ef28766c6bda50a1cf4a1bc549719f39a4cc5cf4
Srinivasan Jayaraman <45791344+Srinivasan87@users.noreply.github.com>
2019-04-07 03:46:52 -0400

    9 Getting Data from Event

---
+++ Basic Concepts/9 Getting Data from Event/index.js
@@ -0,0 +1,17 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        counter: 0,
+        x:0,
+        y:0
+    },
+    methods:{
+        increase: function(){
+            this.counter++;
+        },
+        updatecoordinats:function(){
+            this.x=event.clientX;
+            this.y=event.clientY;
+        },
+    }
+});
